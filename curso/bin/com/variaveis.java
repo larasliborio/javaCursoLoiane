e8ce74593a302564b503c3813e9aa1da88ded666
larasliborio <larasousaliborio@gmail.com>
2022-09-24 16:24:45 +0100

    aula 10

---
+++ curso/bin/com/variaveis.java
@@ -0,0 +1,23 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+        public static void main(String []  args){
+        // convenção java
+        Int idade = 20;
+        
+        String nome ="loiane";
+        
+        String nomeDoMeuCacharro ="totó";
+        
+        idade = 25;
+        
+        
+        system.out.println("Idade = " + idade);
+        
+        system.out.println("Nome = " + nome);
+        }
+        
+        
+        
+    }
