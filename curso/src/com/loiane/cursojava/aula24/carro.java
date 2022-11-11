cb1e877f0aa5913e4130fb8ef69cfd6044cde7db
larasliborio <larasousaliborio@gmail.com>
2022-11-11 18:53:05 +0000

    aula24

---
+++ curso/src/com/loiane/cursojava/aula24/carro.java
@@ -0,0 +1,12 @@
+package com.loiane.cursojava.aula24;
+
+public class carro {
+     
+  String marca;  
+ String modelo;
+int numPassageiros;
+double capConbustivel;
+double consumoConbustivel;
+
+
+}
---
+++ curso/src/com/loiane/cursojava/aula24/testeCarro.java
@@ -0,0 +1,31 @@
+package com.loiane.cursojava.aula24;
+
+public class testeCarro {
+    public static void main(String[] args) {
+
+   carro van =new carro();
+van.marca ="fiat";
+van.modelo ="ducato";
+
+van.numPassageiros =10;
+van.capConbustivel =100;
+van.consumoConbustivel =0.2;
+
+System.out.println(van.marca);
+System.out.println(van.modelo);
+
+carro fusca =new carro();
+fusca.marca ="vokswagen";
+fusca.modelo ="fusca";
+
+fusca.numPassageiros =4;
+fusca.capConbustivel =30;
+fusca.consumoConbustivel =0.15;
+
+System.out.println(fusca.marca);
+System.out.println(fusca.modelo);
+
+
+
+    }
+}
