dc1518d869db9e874bc42a6593d1f369497ded9a
larasliborio <larasousaliborio@gmail.com>
2022-11-12 13:18:16 +0000

    aula24

--- curso/src/com/loiane/cursojava/aula24/carro.java
+++ curso/src/com/loiane/cursojava/aula24/carro.java
@@ -1,12 +1,17 @@
 package com.loiane.cursojava.aula24;
 
 public class carro {
-     
-  String marca;  
- String modelo;
-int numPassageiros;
-double capConbustivel;
-double consumoConbustivel;
 
+  String marca;
+  String modelo;
+  int numPassageiros;
+  double capConbustivel;
+  double consumoConbustivel;
+
+  void exibirAutonomia() {
+
+    System.out.println(" A autonomia do carro é " + capConbustivel * consumoConbustivel + "km");
+
+  }
 
 }
--- curso/src/com/loiane/cursojava/aula24/testeCarro.java
+++ curso/src/com/loiane/cursojava/aula24/testeCarro.java
@@ -3,29 +3,27 @@ package com.loiane.cursojava.aula24;
 public class testeCarro {
     public static void main(String[] args) {
 
-   carro van =new carro();
-van.marca ="fiat";
-van.modelo ="ducato";
+        carro van = new carro();
+        van.marca = "fiat";
+        van.modelo = "ducato";
 
-van.numPassageiros =10;
-van.capConbustivel =100;
-van.consumoConbustivel =0.2;
+        van.numPassageiros = 10;
+        van.capConbustivel = 100;
+        van.consumoConbustivel = 0.2;
 
-System.out.println(van.marca);
-System.out.println(van.modelo);
+        System.out.println(van.marca);
+        System.out.println(van.modelo);
 
-carro fusca =new carro();
-fusca.marca ="vokswagen";
-fusca.modelo ="fusca";
+        carro fusca = new carro();
+        fusca.marca = "vokswagen";
+        fusca.modelo = "fusca";
 
-fusca.numPassageiros =4;
-fusca.capConbustivel =30;
-fusca.consumoConbustivel =0.15;
+        fusca.numPassageiros = 4;
+        fusca.capConbustivel = 30;
+        fusca.consumoConbustivel = 0.15;
 
-System.out.println(fusca.marca);
-System.out.println(fusca.modelo);
-
-
+        System.out.println(fusca.marca);
+        System.out.println(fusca.modelo);
 
     }
 }
