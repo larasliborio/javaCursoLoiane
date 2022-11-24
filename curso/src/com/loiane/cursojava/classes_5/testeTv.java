76514f064b5ed773aee9e07646b0b87320c76ab1
larasliborio <larasousaliborio@gmail.com>
2022-11-24 17:13:39 +0000

    classes 2

--- curso/src/com/loiane/cursojava/classes_5/testeTv.java
+++ curso/src/com/loiane/cursojava/classes_5/testeTv.java
@@ -3,20 +3,38 @@ package com.loiane.cursojava.classes_5;
 public class testeTv {
     public static void main(String[] args) {
 
-        tv nesCafeteira = new tv();
-        nesCafeteira.tipo = "tv";
-         nesCafeteira.nome = "sansung";
+        tv sansungTv = new tv();
+        sansungTv.tipo = "tv";
+         sansungTv.nome = "sansung";
     
-          nesCafeteira.valor = 3500;
-         nesCafeteira.durabilidade = 10 ;
-          nesCafeteira.k = "4 k";
+          sansungTv.valor = 3500;
+         sansungTv.durabilidade = 10 ;
+          sansungTv.k = "4 k";
     
-            System.out.println(nesCafeteira.tipo);
-            System.out.println(nesCafeteira.nome);
-            System.out.println(nesCafeteira.valor);
-            System.out.println(nesCafeteira.durabilidade +" anos" +" (depende da pessoa)");
-            System.out.println(nesCafeteira.k);
+            System.out.println(sansungTv.tipo);
+            System.out.println(sansungTv.nome);
+            System.out.println(sansungTv.valor);
+            System.out.println(sansungTv.durabilidade +" anos" +" (depende da pessoa)");
+            System.out.println(sansungTv.k);
     
     
+
+            tv filipsTv = new tv();
+             filipsTv.tipo = "tv";
+             filipsTv.nome = " filips";
+        
+             filipsTv .valor = 3500;
+             filipsTv.durabilidade = 10 ;
+              filipsTv.k = "4 k";
+        
+               
+                System.out.println(filipsTv.nome);
+              
+        
+
+
+
+
+
     }
 }
