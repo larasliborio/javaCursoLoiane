d9c26bc4bf86baccc18822c372711de8d5b8cb89
larasliborio <larasousaliborio@gmail.com>
2023-01-04 19:28:06 +0000

    Generate getters and setters

---
+++ curso/src/com/loiane/cursojava/aula32/carro.java
@@ -0,0 +1,13 @@
+package com.loiane.cursojava.aula32;
+
+class carro {
+
+    
+
+
+
+
+
+
+    
+}
---
+++ curso/src/com/loiane/cursojava/aula32/testeCarro.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.aula32;
+
+import javax.xml.crypto.dsig.spec.HMACParameterSpec;
+
+public class testeCarro {
+    public static void main(String[] args) {
+   
+
+carro van = new carro();
+van.setMarca("fiat");
+System.out.println(van.getMarca());
+
+
+
+    }
+}
