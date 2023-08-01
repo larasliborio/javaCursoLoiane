9acd59fe1c20aba40c683ee05ed9e817c2171eff
larasliborio <larasousaliborio@gmail.com>
2023-08-01 15:37:16 +0100

    aula56

---
+++ curso/src/com/loiane/cursojava/class56/TesteEnum.java
@@ -0,0 +1,13 @@
+package com.loiane.cursojava.class56;
+
+
+public class TesteEnum {
+        public static void main(String[]args) {
+
+        DiaSemana dia =DiaSemana.SEGUNDA ;        
+                System.out.println(dia.toString() + "-" + dia.getValor());
+
+//Data data = new Data(1, 4, 2016, DiaSemana.SEXTA);
+//System.out.println(Genero.FEMININO);
+        }
+        }
